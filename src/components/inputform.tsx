33f8173efcf72b65367e48b2c0b0cd4f395ad431
Khum <khumchhetri1@gmail.com>
2024-10-01 08:45:45 +0545

    v1

---
+++ src/components/inputform.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const InputForm = () => {
+    return (
+        <div>
+            <form action="">
+                <div>
+                    <label htmlFor="">Name</label>
+                    <input type="text" />
+                </div>
+                <button>Ok</button>
+            </form>
+        </div>
+    )
+}
+
+export default InputForm
